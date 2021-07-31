a186c9552c8ad08ebff71d7a9e1d0668ae6fbbf1
Timur Rakov <ermorder@gmail.com>
2021-07-31 10:18:43 +0600

    Initial Commit

---
+++ Problems/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Problems
+//
+//  Created by Timur Rakov on 31.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Problems/ProblemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProblemsApp.swift
+//  Problems
+//
+//  Created by Timur Rakov on 31.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ProblemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
