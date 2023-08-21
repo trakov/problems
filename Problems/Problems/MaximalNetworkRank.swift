5558f36eeea0a6fc9f89b4fe6a4f6a69bee08bf6
Timur Rakov <ermorder@gmail.com>
2023-08-21 15:16:43 +0300

    MaximalNetworkRank

--- Problems/Problems/MaximalNetworkRank.swift
+++ Problems/Problems/MaximalNetworkRank.swift
@@ -1,5 +1,5 @@
 class MaximalNetworkRank {
-    func maximalNetworkRank(_ n: Int, _ roads: [[Int]]) -> Int {
+    func maximalNetworkRank2(_ n: Int, _ roads: [[Int]]) -> Int {
         var graph = Array(repeating: Set<Int>(), count: n)
         for road in roads {
             graph[road[0]].insert(road[1])
@@ -15,6 +15,64 @@ class MaximalNetworkRank {
         }
         return result
     }
+    
+    func maximalNetworkRank(_ n: Int, _ roads: [[Int]]) -> Int {
+        var graph = Array(repeating: Set<Int>(), count: n)
+        for road in roads {
+            graph[road[0]].insert(road[1])
+            graph[road[1]].insert(road[0])
+        }
+        var buckets = Array(repeating: [Int](), count: n)
+        for (city, dests) in graph.enumerated() {
+            buckets[dests.count].append(city)
+        }
+        var result = 0
+        
+        func isAllConnected<T: Collection>(cities: T, to city: Int) -> Bool where T.Element == Int {
+            for c in cities {
+                if !graph[c].contains(city) {
+                    return false
+                }
+            }
+            return true
+        }
+        
+        func isAllConnected(cities: [Int]) -> Bool {
+            let n = cities.count
+            for i in 0..<n - 1 {
+                let city = cities[i]
+                if !isAllConnected(cities: cities[i+1..<n], to: city) {
+                    return false
+                }
+            }
+            return true
+        }
+        
+        var firstCity: Int?
+        for i in (0..<n).reversed() where !buckets[i].isEmpty {
+            let bucket = buckets[i]
+            if result == 0 {
+                if bucket.count == 1 {
+                    firstCity = bucket[0]
+                    result = i
+                } else {
+                    result = 2 * i
+                    if isAllConnected(cities: bucket) {
+                        result -= 1
+                    }
+                    return result
+                }
+            } else {
+                result += i
+                if isAllConnected(cities: bucket, to: firstCity!) {
+                    result -= 1
+                }
+                return result
+            }
+        }
+        return result
+    }
+
     
     func tests() {
         print(maximalNetworkRank(4, [[0,1],[0,3],[1,2],[1,3]])) // 4
