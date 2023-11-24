6041a11efe6b1c6b2ebd2d51ab132d4943a70b3c
Timur Rakov <ermorder@gmail.com>
2023-11-24 13:14:14 +0300

    ArithmeticSubarrays

--- Problems/Problems/ArithmeticSubarrays.swift
+++ Problems/Problems/ArithmeticSubarrays.swift
@@ -3,6 +3,37 @@ class ArithmeticSubarrays {
         var result = Array(repeating: true, count: l.count)
         for (i, left) in l.enumerated() {
             let right = r[i]
+            var minVal = Int.max
+            var maxVal = Int.min
+            for k in left...right {
+                minVal = min(minVal, nums[k])
+                maxVal = max(maxVal, nums[k])
+            }
+            let n = right - left + 1
+            guard (maxVal - minVal) % (n - 1) == 0 else {
+                result[i] = false
+                continue
+            }
+            let diff = (maxVal - minVal) / (n - 1)
+            guard diff > 0 else { continue }
+            var count: [Int?] = Array(repeating: nil, count: n)
+            for j in left...right {
+                let num = nums[j]
+                let k = (num - minVal) / diff
+                guard (num - minVal) % diff == 0 && count[k] == nil else {
+                    result[i] = false
+                    break
+                }
+                count[k] = num
+            }
+        }
+        return result
+    }
+
+    func checkArithmeticSubarrays2(_ nums: [Int], _ l: [Int], _ r: [Int]) -> [Bool] {
+        var result = Array(repeating: true, count: l.count)
+        for (i, left) in l.enumerated() {
+            let right = r[i]
             let sub = nums[left...right].sorted()
             guard sub.count > 2 else {
                 continue
@@ -23,6 +54,11 @@ class ArithmeticSubarrays {
             [-12,-9,-3,-12,-6,15,20,-25,-20,-15,-10],
             [0,1,6,4,8,7],
             [4,4,9,7,9,10])) // [false,true,false,false,true,true]
+        print(checkArithmeticSubarrays(
+            [-3,-6,-8,-4,-2,-8,-6,0,0,0,0],
+            [5,4,3,2,4,7,6,1,7],
+            [6,5,6,3,7,10,7,4,10])
+        )
     }
 }
 
