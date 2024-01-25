4670f15240cbef638ae31b01745192c8bba638b5
Timur Rakov <ermorder@gmail.com>
2024-01-25 08:47:44 +0300

    LongestCommonSubsequence

--- Problems/Problems/LongestCommonSubsequence.swift
+++ Problems/Problems/LongestCommonSubsequence.swift
@@ -1,5 +1,21 @@
 class LongestCommonSubsequence {
     func longestCommonSubsequence(_ text1: String, _ text2: String) -> Int {
+        var dp = Array(repeating: 0, count: text2.count + 1)
+        for (i, c1) in text1.enumerated() {
+            var newDP = Array(repeating: 0, count: text2.count + 1)
+            for (j, c2) in text2.enumerated() {
+                if c1 == c2 {
+                    newDP[j+1] = dp[j] + 1
+                } else {
+                    newDP[j+1] = max(dp[j+1], newDP[j])
+                }
+            }
+            dp = newDP
+        }
+        return dp.last!
+    }
+
+    func longestCommonSubsequence1(_ text1: String, _ text2: String) -> Int {
         let text1 = Array(text1)
         let text2 = Array(text2)
         var dp = Array(repeating: Array(repeating: 0, count: text2.count + 1), count: text1.count + 1)
