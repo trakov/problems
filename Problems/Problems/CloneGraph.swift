f408816c8038f9e12b646d519fd703abb2991191
Timur Rakov <ermorder@gmail.com>
2024-05-14 16:41:51 +0300

    CloneGraph: rewritten solution

--- Problems/Problems/CloneGraph.swift
+++ Problems/Problems/CloneGraph.swift
@@ -1,29 +1,42 @@
 class CloneGraph {
-//    func cloneGraph(_ node: Node?) -> Node? {
-//        var visited = [Int: Node]()
-//        return dfs(node, visited: &visited)
-//    }
-//    
-//    func dfs(_ node: Node?, visited: inout [Int: Node]) -> Node? {
-//        guard let node = node else { return nil }
-//
-//        let newNode = Node(node.val)
-//        visited[node.val] = newNode
-//
-//        for neighbor in node.neighbors {
-//            guard let neighbor = neighbor else { continue }
-//
-//            if let n = visited[neighbor.val] {
-//                newNode.neighbors.append(n)
-//            } else {
-//                let n = dfs(neighbor, visited: &visited)
-//                newNode.neighbors.append(n)
-//            }
-//        }
-//
-//        return newNode
-//    }
+    public class Node {
+        public var val: Int
+        public var neighbors: [Node?]
+        public init(_ val: Int) {
+            self.val = val
+            self.neighbors = []
+        }
+    }
 
+    func cloneGraph(_ node: Node?) -> Node? {
+        var visited: [Int: Node] = [:] // val: new_node
+        func dfs(_ node: Node?) -> Node? {
+            guard let node else { return nil }
+            guard visited[node.val] == nil else { return visited[node.val] }
+            let newNode = Node(node.val)
+            visited[node.val] = newNode
+            newNode.neighbors = node.neighbors.map { dfs($0) }
+            return newNode
+        }
+        return dfs(node)
+    }
+
+    func cloneGraph2(_ node: Node?) -> Node? {
+        var visited: [Int: Node] = [:] // val: new_node
+        func dfs(_ node: Node?) -> Node? {
+            guard let node else { return nil }
+            let newNode = Node(node.val)
+            visited[node.val] = newNode
+            for neighbor in node.neighbors {
+                guard let neighbor else { continue }
+                let n = visited[neighbor.val] ?? dfs(neighbor)
+                newNode.neighbors.append(n)
+            }
+            return newNode
+        }
+        return dfs(node)
+    }
+    
     func tests() {
         
     }
