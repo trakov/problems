6ce0472052c8ec1abbaefb16434d6a6dcd6c3033
Timur Rakov <ermorder@gmail.com>
2024-08-12 06:11:00 +0300

    KthLargestElementInAStream: heap solution

--- Problems/Problems/KthLargestElementInAStream.swift
+++ Problems/Problems/KthLargestElementInAStream.swift
@@ -1,3 +1,38 @@
+class KthLargestElementInAStreamHeap {
+    private var heap = Heap<Int>(sort: <)
+    private let k: Int
+    
+    init(_ k: Int = 0, _ nums: [Int] = []) {
+        for num in nums {
+            heap.insert(num)
+            if heap.count > k {
+                heap.remove()
+            }
+        }
+        self.k = k
+    }
+    
+    func add(_ val: Int) -> Int {
+        if heap.count < k || heap.peek()! < val {
+            heap.insert(val)
+            if heap.count > k {
+                heap.remove()
+            }
+        }
+        return heap.peek()!
+    }
+
+    
+    func tests() {
+        let t = KthLargestElementInAStream(1, [])
+        print(t.add(3)) // 4
+        print(t.add(5)) // 5
+        print(t.add(10)) // 5
+        print(t.add(9)) // 8
+        print(t.add(4)) // 8
+    }
+}
+
 class KthLargestElementInAStream {
     private var nums: [Int]
     private let k: Int
