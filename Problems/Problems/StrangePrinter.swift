99fba4dc14a1e82b4a3e3686e0e9616e21e09832
Timur Rakov <ermorder@gmail.com>
2024-08-21 13:29:44 +0300

    StrangePrinter: tabulation

--- Problems/Problems/StrangePrinter.swift
+++ Problems/Problems/StrangePrinter.swift
@@ -1,5 +1,39 @@
 class StrangePrinter {
     func strangePrinter(_ s: String) -> Int {
+        var array: [Character] = [s.first!]
+        for c in s.dropFirst() where array.last! != c {
+            array.append(c)
+        }
+        let n = array.count
+        guard n > 1 else { return n }
+        var minTurns = Array(repeating: Array(repeating: 0, count: n), count: n)
+        for i in 0..<n {
+            minTurns[i][i] = 1
+        }
+        for length in 2...n {
+            var start = 0
+            while start + length - 1 < n {
+                let end = start + length - 1
+                minTurns[start][end] = length
+                for split in 0..<length - 1 {
+                    var totalTurns =
+                    minTurns[start][start + split] +
+                    minTurns[start + split + 1][end]
+                    if array[start + split] == array[end] {
+                        totalTurns -= 1
+                    }
+                    minTurns[start][end] = min(
+                        minTurns[start][end],
+                        totalTurns
+                    )
+                }
+                start += 1
+            }
+        }
+        return minTurns[0][n - 1]
+    }
+    
+    func strangePrinter1(_ s: String) -> Int {
         let n = s.count
         let s = Array(s)
         var dp = Array(repeating: Array(repeating: -1, count: n), count: n)
