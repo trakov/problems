24cc14589bd33b684707cd7b2f7cad52b66cf150
Timur Rakov <ermorder@gmail.com>
2024-08-25 15:24:51 +0300

    BinaryTreePostorderTraversal: + 2 solutions

--- Problems/Problems/BinaryTreePostorderTraversal.swift
+++ Problems/Problems/BinaryTreePostorderTraversal.swift
@@ -1,5 +1,46 @@
 class BinaryTreePostorderTraversal {
+    // single stack
     func postorderTraversal(_ root: TreeNode?) -> [Int] {
+        var result: [Int] = []
+        var stack: [TreeNode] = []
+        var node = root
+        var prev: TreeNode?
+        while node != nil || !stack.isEmpty {
+            while node != nil {
+                stack.append(node!)
+                node = node?.left
+            }
+            node = stack.last
+            if node?.right == nil || node?.right === prev {
+                result.append(node!.val)
+                stack.removeLast()
+                prev = node
+                node = nil
+            } else {
+                node = node?.right
+            }
+        }
+        return result
+    }
+    
+    // opposite of preorder
+    func postorderTraversal1(_ root: TreeNode?) -> [Int] {
+        var result: [Int] = []
+        var stack: [TreeNode] = []
+        var node = root
+        while node != nil || !stack.isEmpty {
+            while node != nil {
+                result.append(node!.val)
+                stack.append(node!)
+                node = node?.right
+            }
+            node = stack.removeLast()
+            node = node?.left
+        }
+        return result.reversed()
+    }
+
+    func postorderTraversal2(_ root: TreeNode?) -> [Int] {
         guard root != nil else { return [] }
         var result: [Int] = []
         var stack: [TreeNode] = [root!]
