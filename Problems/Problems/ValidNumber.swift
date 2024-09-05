6af1a6718e6e6d9b97cc5a37b3ccef0d7c257754
Timur Rakov <ermorder@gmail.com>
2024-09-05 19:11:40 +0300

    ValidNumber: +1 solution

--- Problems/Problems/ValidNumber.swift
+++ Problems/Problems/ValidNumber.swift
@@ -1,6 +1,36 @@
 class ValidNumber {
     func isNumber(_ s: String) -> Bool {
         let s = Array(s.lowercased())
+        var numSeen = false
+        var eSeen = false
+        var dotSeen = false
+        for (i, c) in s.enumerated() {
+            if c.isNumber {
+                numSeen = true
+            } else if c == "+" || c == "-" {
+                if i > 0 && s[i-1] != "e" {
+                    return false
+                }
+            } else if c == "e" {
+                if eSeen || !numSeen {
+                    return false
+                }
+                eSeen = true
+                numSeen = false
+            } else if c == "." {
+                if dotSeen || eSeen {
+                    return false
+                }
+                dotSeen = true
+            } else {
+                return false
+            }
+        }
+        return numSeen
+    }
+    
+    func isNumber2(_ s: String) -> Bool {
+        let s = Array(s.lowercased())
         var pointSeen = false
         var eSeen = false
         var numberSeen = false
